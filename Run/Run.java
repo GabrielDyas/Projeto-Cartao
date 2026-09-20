3b6176f751b6bfba4c80edfa0bc9c6254a4a06db
Arthur Polak Furman <arthur.polak.furman@gmail.com>
2026-09-20 15:00:42 -0300

    coisa

--- Run/Run.java
+++
@@ -1,9 +0,0 @@
-package Run;
-import Cliente.Cliente;
-
-public class Run {
-    public static void main(String[] args) {
-        // Cliente cliente = new Cliente();
-        // cliente.setState(new cliente.States.Comprando());
-    }
-}
